4934f6312d7e684d1392a788be6138143d2d28d9
Orl967 <2410817210017@ulm.ac.id>
2026-05-21 10:55:22 +0800

    WEB2-01-02 feat: create sports categories migration model and seeder

--- app/Models/SportsCategory.php
+++ app/Models/SportsCategory.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class SportsCategory extends Model
 {
-    //
+    protected $fillable = [
+        'name',
+        'slug',
+        'icon',
+        'is_active',
+    ];
 }
