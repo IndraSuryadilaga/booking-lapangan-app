2e8201a2fee5bab25b91a27b7991d9795d4705b3
IndraSuryadilaga <indrasryd@gmail.com>
2026-05-28 17:38:16 +0800

    fix: mengurutkan seeder

---
+++ database/migrations/2026_02_02_120227_create_venue_sport_categories_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('venue_sport_categories', function (Blueprint $table) {
+            $table->foreignId('venue_id')
+                ->constrained()
+                ->onDelete('cascade');
+
+            $table->foreignId('sports_category_id')
+                ->constrained()
+                ->onDelete('cascade');
+
+            $table->primary(['venue_id', 'sports_category_id']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('venue_sport_categories');
+    }
+};
